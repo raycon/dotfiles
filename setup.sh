34d4c3b359a989495576d5787bd895e6a6e35924
raegon <raegon@gmail.com>
2021-03-26 14:56:10 +0900

    Add .ideavimrc

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@ ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/coc-settings.json ~/.config/nvim/coc-settings.json
+ln -s ~/dotfiles/.ideavimrc ~/.ideavimrc
 
